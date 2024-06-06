f9d978e709bed443fe4b4d228225aab858be78bb
jesse <jesse.yowell@gmail.com>
2024-06-05 21:31:57 -0700

    Adding scanf to this wonderful program

--- dimensional-weight.c
+++ dimensional-weight.c
@@ -1,18 +1,26 @@
+
+/* Computes the dimensional weight of a box from input provided by the user */
+
 #include <stdio.h>
+#define INCHES_PER_POUND 166
 
 int main(void) {
 
     int length, width, height, volume, weight;
 
-    height = 8;
-    length = 12;
-    width = 10;
-    volume = length * width * height;
-    weight = (volume + 165) / 166;
+      printf("Enter the height of the box: ");
+      scanf("%d", &height);
+      printf("Enter the length of the box: ");
+      scanf("%d",&length);
+      printf("Enter the width of the box: ");
+      scanf("%d", &width);
+      
+      volume = length * width * height;
+      weight = (volume + INCHES_PER_POUND - 1) / INCHES_PER_POUND;
 
-    printf("Dimensions: %dx,%dx,%dx\n", length, width, height);
-    printf("Volume (cubic inches): %d\n", volume);
-    printf("Dimensional weight (pounds): %d\n", weight);
+      printf("Dimensions: %d length x %d width x %d height \n", length, width, height);
+      printf("Volume (cubic inches): %d\n", volume);
+      printf("Dimensional weight (pounds): %d\n", weight);
 
     return 0;
 
